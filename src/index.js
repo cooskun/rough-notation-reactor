295d8cdaf6a63ccfda9554b416d95b4b06ca1778
coskunbaris <coskun.bris@gmail.com>
2020-06-01 22:15:26 +0200

    All props applied for annotation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,59 @@
-import React, {Component} from 'react'
+import React, { useEffect, useState, useRef } from 'react'
+import PropTypes from 'prop-types'
+import { annotate } from 'rough-notation'
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
+const RoughNotation = ({
+  type = 'underline',
+  animate,
+  animationDelay,
+  animationDuration,
+  color,
+  strokeWidth,
+  padding,
+  as: Element = 'span',
+  children,
+  ...props
+}) => {
+  const ref = useRef()
+
+  useEffect(() => {
+    const annotation = annotate(ref.current, {
+      type,
+      animate,
+      animationDuration,
+      animationDelay,
+      color,
+      strokeWidth,
+      padding,
+    })
+    annotation.show()
+  }, [])
+
+  return (
+    <Element ref={ref} {...props}>
+      {children}
+    </Element>
+  )
 }
+
+RoughNotation.propTypes = {
+  color: PropTypes.string,
+  animate: PropTypes.bool,
+  animationDelay: PropTypes.number,
+  animationDuration: PropTypes.number,
+  as: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.element]),
+  padding: PropTypes.number,
+  strokeWidth: PropTypes.number,
+  type:
+    PropTypes.oneOf[
+      ('underline',
+      'box',
+      'circle',
+      'highlight',
+      'strike-through',
+      'crossed-off')
+    ],
+}
+
+export default RoughNotation
